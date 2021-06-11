27568902c3f4a7c8ae96084238735c19cdb78d7a
Morris Buel <morris.buel@gmail.com>
2021-06-10 21:22:57 -0700

    Work completed!

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'static_pages#index'
+  get '/posts', to: 'posts#index'
 
   # <http-request-type> <url>, to: '<controller-name>#<method-in-controller>'
   post '/posts', to: 'posts#create'
   post '/users', to: 'users#create'
+
+  #modifications by post id
+  put '/posts', to: 'posts#modify'
+  delete '/posts', to: 'posts#delete'
 end
